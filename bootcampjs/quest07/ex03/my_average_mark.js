e44cf399b2d247e2ffc585a70f5f9927a8b23787
Pearlicia <feliciaebikon@gmail.com>
2023-01-28 11:20:21 +0100

    add bouncing box

--- bootcampjs/quest07/ex03/my_average_mark.js
+++ bootcampjs/quest07/ex03/my_average_mark.js
@@ -1,24 +1,61 @@
-const input = [
-    {"string": "John", "integer": 7},
-    {"string": "Margot", "integer": 8},
-    {"string": "Jules", "integer": 4},
-    {"string": "Marco", "integer": 19}
-]
+// Assignment:
+// You have a test for a class and we want to know the average results for the class. Write a function that takes an array of hash with all of the grades/marks for a given test and returns an average grade for the entire class.
+// Input are in JSON (be careful ruby users, "string": is not a symbol)
+// Example 00: (In Javascript)
+// John, Margot, Jules, and Marco are in a class together.
 
-// function my_average_mark(arrobj) {
-//     const { length } = arrobj;
-//     return arrobj.reduce((str, int) => {
-//        return str + (int.integer/length);
-//     }, 0);
-//  };
-//  console.log(my_average_mark(input));
-console.log(Object.entries(input)[2].values());
+// Input: [
+//         {"string": "John", "integer": 7},
+//         {"string": "Margot", "integer": 8},
+//         {"string": "Jules", "integer": 4},
+//         {"string": "Marco", "integer": 19}
+//        ]
 
-// const myArray = [{x:100}, {x:200}, {x:300}];
-// function my_average_mark(arrobj) {
-//     var values = arrobj.map(function(o) { return o.; });
-//     const sum = arrobj.map(element => element.x).reduce((a, b) => a + b, 0);
-//     console.log(sum); // 600 = 0 + 100 + 200 + 300
-//     const average = sum / myArray.length;
-//     console.log(average); // 2
-// }
+// Output: 9.5
+// Example 01
+
+// Input: [
+//         {"string": "Quentin", "integer": 1},
+//         {"string": "Fred", "integer": 1},
+//         {"string": "Julia", "integer": 18},
+//         {"string": "stephanie", "integer": 13}
+//        ]
+// Output: 8.3
+// Example 02
+
+// Input: []
+// Output: 0.0
+// Tips
+// (In Javascript)
+// For javascript user in order to get the right precision of the floating number, use toFixed().
+// Google the following: Float variable type
+// Google the following: what is a Hash (look at dictionary or technical definition and use cases)
+// Google the following: Object.keys(hash)
+
+
+/*
+**
+** QWASAR.IO -- my_average_mark
+**
+**
+** @param {String_integer[]} param_1
+** @return {float}
+
+**
+*/
+
+
+function my_average_mark(score){
+    let result = 0;
+    if (score.length === 0){
+        return 0.0;
+    }
+   
+    for (let i = 0; i < score.length; i++){
+       
+        result += score[i]["integer"];
+        average = result / score.length
+    }
+
+    return average.toFixed(1)
+}
