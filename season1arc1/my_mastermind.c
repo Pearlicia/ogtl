39757d1e74b3145493c27619925b1a2d73dc9cde
Pearlicia <feliciaebikon@gmail.com>
2023-02-07 14:45:58 +0100

    master mind

--- season1arc1/my_mastermind.c
+++ season1arc1/my_mastermind.c
@@ -73,6 +73,166 @@ int main(int argc, char *argv[]) {
   exit(EXIT_SUCCESS);
 }
 
-// This code uses the getopt function to parse the options -c and -t. If no code is specified, a random code 
+// 
+This code uses the getopt function to parse the options -c and -t. If no code is specified, a random code 
 // is generated using the generate_code function. The player's guess is read from the standard input using
 //  the fgets function. If the input is not 4 characters long, it is considered
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+#include <stdio.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+
+#define MAX_ATTEMPTS 10
+
+char *generate_code() {
+    char *code = malloc(5);
+    int i;
+    for (i = 0; i < 4; i++) {
+        code[i] = (char)(rand() % 9 + '0');
+    }
+    code[4] = '\0';
+    return code;
+}
+
+int main(int argc, char *argv[]) {
+    srand(time(NULL));
+    int attempts = MAX_ATTEMPTS;
+    char *code = generate_code();
+    int opt;
+    while (optind < argc) {
+        switch (argv[optind][1]) {
+            case 'c':
+                code = &argv[optind][2];
+                break;
+            case 't':
+                attempts = atoi(&argv[optind][2]);
+                break;
+            default:
+                printf("Usage: %s [-c CODE] [-t ATTEMPTS]\n", argv[0]);
+                exit(EXIT_FAILURE);
+        }
+        optind++;
+    }
+    printf("Will you find the secret code?\nPlease enter a valid guess\n");
+    char guess[5];
+    int well_placed, misplaced;
+    int i;
+    int j;
+    for (i = 0; i < attempts; i++) {
+        printf("---\nRound %d\n> ", i + 1);
+        int j = 0;
+        char c;
+        int read_result = read(STDIN_FILENO, &c, 1);
+        while(read_result == 1 && c != '\n' && j < 4) {
+            guess[j++] = c;
+            read_result = read(STDIN_FILENO, &c, 1);
+        }
+        guess[j] = '\0';
+        well_placed = misplaced = 0;
+        for (j = 0; j < 4; j++) {
+            if (guess[j] == code[j]) {
+                well_placed++;
+            } else {
+                int k;
+                for (k = 0; k < 4; k++) {
+                    if (guess[j] == code[k]) {
+                        misplaced++;
+                        break;
+                    }
+                }
+            }
+        }
+   if (well_placed == 4) {
+      printf("Congratz! You did it!\n");
+      exit(EXIT_SUCCESS);
+    }
+    printf("Well placed pieces: %d\nMisplaced pieces: %d\n", well_placed, misplaced);
+  }
+  printf("You failed to find the secret code. Better luck next time!\n");
+  exit(EXIT_SUCCESS);
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define MAX_ATTEMPTS 10
+
+char *generate_code() {
+  char *code = malloc(5);
+  int i;
+  for (i = 0; i < 4; i++) {
+    code[i] = (char)(rand() % 9 + '0');
+  }
+  code[4] = '\0';
+  return code;
+}
+
+int main(int argc, char *argv[]) {
+  srand(time(NULL));
+  int attempts = MAX_ATTEMPTS;
+  char *code = generate_code();
+  int opt;
+  while ((opt = getopt(argc, argv, "c:t:")) != -1) {
+    switch (opt) {
+      case 'c':
+        code = optarg;
+        break;
+      case 't':
+        attempts = atoi(optarg);
+        break;
+      default:
+        printf("Usage: %s [-c CODE] [-t ATTEMPTS]\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+  }
+  printf("Will you find the secret code?\nPlease enter a valid guess\n");
+  char guess[5];
+  int well_placed, misplaced;
+  int i;
+  for (i = 0; i < attempts; i++) {
+    printf("---\nRound %d\n> ", i + 1);
+    if (scanf("%s", guess) != 1) {
+      printf("Exited due to End of File\n");
+      exit(EXIT_SUCCESS);
+    }
+    if (strlen(guess) != 4) {
+      printf("Wrong input!\n");
+      i--;
+      continue;
+    }
+    well_placed = misplaced = 0;
+    int j;
+    for (j = 0; j < 4; j++) {
+      if (guess[j] == code[j]) {
+        well_placed++;
+      } else {
+        int k;
+        for (k = 0; k < 4; k++) {
+          if (guess[j] == code[k]) {
+            misplaced++;
+            break;
+          }
+        }
+      }
+    }
+    if (well_placed == 4) {
+      printf("Congratz! You did it!\n");
+      exit(EXIT_SUCCESS);
+    }
+    printf("Well placed pieces: %d\nMisplaced pieces: %d\n", well_placed, misplaced);
+  }
+  printf("You failed to find the secret code. Better luck next time!\n");
+  exit(EXIT_SUCCESS);
+}
+
