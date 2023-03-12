f6d34dadda61c7a62aa21167d8ed1f538fd80247
Pearlicia <feliciaebikon@gmail.com>
2023-03-12 22:37:37 +0100

    my basecamp

---
+++ ruby-tutorial/mybasecamp/mybasecampone/config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # get 'pages/home'
+  get 'login', to: 'pages#login'
+  # get 'pages/login'
+  get 'pages/register'
+  get 'register', to: 'pages#register'
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "pages#home"
+end
