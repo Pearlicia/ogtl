c65e8796a097d01dcaa05ec1f2fd54d84500b8dd
Pearlicia <feliciaebikon@gmail.com>
2023-03-16 18:38:40 +0100

    add project functionalities

--- ruby-tutorial/mybasecamp/mybasecampone/app/models/project.rb
+++ ruby-tutorial/mybasecamp/mybasecampone/app/models/project.rb
@@ -1,2 +1,5 @@
 class Project < ApplicationRecord
+    validates :name, presence: true, length: {minimum: 5, maximum: 50}
+    validates :description, presence: true, length: {minimum: 10, maximum: 1000}
+
 end
