9cdebc80859f19c0279c35ff6aa7719ff370dfec
Pearlicia <feliciaebikon@gmail.com>
2023-03-16 17:47:59 +0100

    add project functionalities

--- ruby-tutorial/mybasecamp/mybasecampone/config/routes.rb
+++ ruby-tutorial/mybasecamp/mybasecampone/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  resources :projects
   # get 'pages/home'
   get 'login', to: 'pages#login'
   # get 'pages/login'
-  get 'pages/register'
+  # get 'pages/register'
   get 'register', to: 'pages#register'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
