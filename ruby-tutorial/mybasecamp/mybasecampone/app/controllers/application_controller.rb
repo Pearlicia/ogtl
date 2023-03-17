657f332d9f9d6c50d06760db3ed30f20e2107617
Pearlicia <feliciaebikon@gmail.com>
2023-03-17 19:32:16 +0100

    design sign up page

--- ruby-tutorial/mybasecamp/mybasecampone/app/controllers/application_controller.rb
+++ ruby-tutorial/mybasecamp/mybasecampone/app/controllers/application_controller.rb
@@ -1,5 +1,18 @@
 class ApplicationController < ActionController::Base
     # ...
+      before_action :configure_permitted_parameters, if: :devise_controller?
+    
+      protected
+    
+      def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      end
+    
+    
+    def configure_sign_up_params
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password])
+    end
+    
   
     def after_sign_in_path_for(resource)
       projects_path
@@ -9,4 +22,5 @@ class ApplicationController < ActionController::Base
       projects_path
     end
   end
+
   
