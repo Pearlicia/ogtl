4b9d28e69dd152c987ccdcf0dcf1646393f1feca
Pearlicia <feliciaebikon@gmail.com>
2023-03-20 09:32:29 +0100

    edit discussion box

--- ruby-tutorial/mybasecamp/mybasecampone/app/javascript/application.js
+++ ruby-tutorial/mybasecamp/mybasecampone/app/javascript/application.js
@@ -1,7 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "font-awesome";
+// import "font-awesome";
 
 
 import "trix"
