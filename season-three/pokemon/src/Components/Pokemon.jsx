2ab81a274c09ffad09d19a8b4d7b42e04fda235f
Pearlicia <feliciaebikon@gmail.com>
2023-03-25 17:33:07 +0100

    add tab

--- season-three/pokemon/src/Components/Pokemon.jsx
+++ season-three/pokemon/src/Components/Pokemon.jsx
@@ -1,14 +1,24 @@
 import React, { useEffect, useState } from "react";
-import { Typography, Link, CircularProgress, Button } from "@material-ui/core";
+import { Typography, Box, Link, Tabs, CircularProgress, Button } from "@material-ui/core";
+import Tab from '@material-ui/core/Tab';
+import TabPanel from '@material-ui/lab/TabPanel';
+import TabContext from '@material-ui/lab/TabContext';
 import { toFirstCharUppercase } from "./constants";
 import axios from "axios";
 import { useParams, useNavigate } from 'react-router-dom';
+import '../App.css';
 
 const Pokemon = (props) => {
   const params = useParams();
   const navigate = useNavigate();
   const { pokemonId } = params;
   const [pokemon, setPokemon] = useState(undefined);
+
+  const [toggle, setToggle] = useState(1);
+
+  function updateToggle(id) {
+    setToggle(id)
+  }
 
   useEffect(() => {
     axios
@@ -23,7 +33,7 @@ const Pokemon = (props) => {
   }, [pokemonId]);
 
   const generatePokemonJSX = (pokemon) => {
-    const { name, id, species, height, weight, types, sprites } = pokemon;
+    const { name, id, species, height, weight, types, moves, stats, abilities, sprites } = pokemon;
     const fullImageUrl = `https://pokeres.bastionbot.org/images/pokemon/${id}.png`;
     const { front_default } = sprites;
     return (
@@ -33,18 +43,57 @@ const Pokemon = (props) => {
         </Typography>
         <img style={{ width: "300px", height: "300px" }} src={front_default} />
         <Typography variant="h3">Pokemon Info</Typography>
-        <Typography>
-          {"Species: "}
-          <Link href={species.url}>{species.name} </Link>
-        </Typography>
-        <Typography>Height: {height} </Typography>
-        <Typography>Weight: {weight} </Typography>
-        <Typography variant="h6"> Types:</Typography>
-        {types.map((typeInfo) => {
-          const { type } = typeInfo;
-          const { name } = type;
-          return <Typography key={name}> {`${name}`}</Typography>;
-        })}
+
+       <TabContext>
+          <Box sx={{ borderBottom: 3, borderColor: 'divider' }}>
+              <Tabs>
+                <Tab label="About" onClick={()=>updateToggle(1)} />
+                <Tab label="Base Stats" onClick={()=>updateToggle(2)} />
+                <Tab label="Evolution" onClick={()=>updateToggle(3)} />
+                <Tab label="Moves" onClick={()=>updateToggle(4)} />
+              </Tabs>
+          </Box>
+          <TabPanel className={toggle === 1 ? "show-content" : "content"}>
+            <Typography>
+              {"Species: "}
+              <Link href={species.url}>{species.name} </Link>
+            </Typography>
+            <Typography>Height: {height} </Typography>
+            <Typography>Weight: {weight} </Typography>
+            <Typography>
+              {"Abilities"}
+              {abilities.map((ability) => (
+              <li key={ability.ability.name}>{ability.ability.name}</li>
+            ))}
+            </Typography>
+          
+          </TabPanel>
+          <TabPanel className={toggle === 2 ? "show-content" : "content"}>
+              {stats.map((stat, index) => (
+                <div key={index}>
+                  <p>{stat.stat.name}: {stat.base_stat}</p>
+                </div>
+              ))}
+
+          <Typography variant="h6"> Types:</Typography>
+                  {types.map((typeInfo) => {
+                    const { type } = typeInfo;
+                    const { name } = type;
+                    return <li key={name}> {`${name}`}</li>;
+                  })}
+          </TabPanel>
+          <TabPanel className={toggle === 3 ? "show-content" : "content"}>
+            Evolution
+          </TabPanel>
+          <TabPanel className={toggle === 4 ? "show-content" : "content"}>
+              {moves.map(move => (
+                <li key={move.move.name}>{move.move.name}</li>
+              ))}
+          </TabPanel>
+
+       </TabContext>
+        
+      
       </>
     );
   };
